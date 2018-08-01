9feae97ca17553b6464d73c812f77ddcb6429a62
felipe.martins <felipe.martins@seduc.ce.gov.br>
2018-08-01 15:00:25 -0300

    Algumas alterações

--- apps/typescript/lesson03-generics/main.ts
+++ apps/typescript/lesson03-generics/main.ts
@@ -4,5 +4,7 @@ import { Dao} from './dao';
 
 let dao: DaoInterface<Person> = new Dao<Person>();
 let person: Person = new Person('Mike');
+let person2: Person = new Person('John');
 
 dao.insert(person);
+dao.insert(person2);
